c8e69248593b65fcc5eb4b35260edb4aeeb5259e
P.Raut <p.raut@serrala.com>
2022-06-14 20:09:32 +0530

    Junit added

---
+++ src/test/java/com/ecom/cart/service/implemetation/CartImplTest.java
@@ -0,0 +1,32 @@
+package com.ecom.cart.service.implemetation;
+
+import com.ecom.cart.entity.Cart;
+import com.ecom.cart.service.implementation.CartImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+public class CartImplTest {
+
+    @InjectMocks
+    private CartImpl cart;
+
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void test() {
+        List<Cart> carts = cart.getAll();
+        Assertions.assertNotNull(carts);
+    }
+
+}
